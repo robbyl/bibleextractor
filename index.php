520a5838e258574444358d892eb51b75f2d30687
Robert <robbyl@ovi.com>
2017-01-30 23:18:09 +0300

    cleaned

--- index.php
+++ index.php
@@ -14,15 +14,24 @@ foreach ($objects as $name => $object) {
     echo $name . "<br/>";
     $html = file_get_html($name);
     $some = array();
-    foreach ($html->find('p', 1) as $element) {
+    
+    foreach ($html->find('span,h3,a,.textAudio,.textHeader,.ym-wbox,.alignCenter,.ym-noprint') as $removed){
+        $removed->outertext='';
+    }
+    
+    
+    foreach ($html->find('p') as $element) {
 
         $some = explode(PHP_EOL, $element->plaintext);
+        
+        echo $element . '<br/>';
 
-        for ($i = 0; $i < count($some); $i++) {
-//            echo preg_replace('/^([0-9]* \w+ )?(.*)$/', '$2', $some[$i]) . '<br/>';
-            echo $some[$i];
-            echo '<br/>';
-        }
+//        for ($i = 0; $i < count($some); $i++) {
+////            echo preg_replace('/^([0-9]* \w+ )?(.*)$/', '$2', $some[$i]) . '<br/>';
+////           echo preg_replace("/^[0-9\\.\\s]+/", "", $some[$i]);
+//            echo $some[i];
+//            echo '<br/>';
+//        }
     }
 }
 
